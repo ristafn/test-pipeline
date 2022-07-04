1abe6d6eb54883ac6a1f7520e52a3244a5ca100a
ristafn <golemmite23@gmail.com>
2022-07-04 12:38:03 +0700

    add handler api/v1/test

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ func main() {
 		json.NewEncoder(w).Encode("hello from test")
 	})
 
+	http.HandleFunc("/api/v1/test", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode("hello from test")
+	})
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
